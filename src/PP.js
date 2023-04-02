9610d77499c42e6847de5ceaa1d0f6ede4fa220e
RISHI CHAARY S <97096580+RISHICHAARY@users.noreply.github.com>
2023-04-02 22:16:18 +0530

    Update PP.js

--- src/PP.js
+++ src/PP.js
@@ -61,7 +61,7 @@ export default function PP(){
                     </div>
                 </div>
         {
-            (Location.state === null)?<Faqs Received={null}/>:(Location.state.user === undefined)?<SideBar Received={null}/>:
+            (Location.state === null)?<Faqs Received={null}/>:(Location.state.user === undefined)?<Faqs Received={null}/>:
             <Faqs Received={ {status: Location.state.status, name: Location.state.name , user:Location.state.user , type:Location.state.type , id:Location.state.id} } />
         }
         </>
