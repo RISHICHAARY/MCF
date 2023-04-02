1ed19e9778af8f008b4e19d762e8753066f4de8b
RISHI CHAARY S <97096580+RISHICHAARY@users.noreply.github.com>
2023-04-02 22:18:46 +0530

    Update admin_dashboard.js

--- src/admin_dashboard.js
+++ src/admin_dashboard.js
@@ -549,7 +549,7 @@ function AdminDashBoard(){
                 </>
             }
             {
-                (Location.state === null)?<Faqs Received={null}/>:(Location.state.user === undefined)?<SideBar Received={null}/>:
+                (Location.state === null)?<Faqs Received={null}/>:(Location.state.user === undefined)?<Faqs Received={null}/>:
                 <Faqs Received={ {status: Location.state.status, name: Location.state.name , user:Location.state.user , type:Location.state.type , id:Location.state.id} } />
             }
         </div>
